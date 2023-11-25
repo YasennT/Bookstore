79e83cf442d2cea7368737fdd735572d3670db7c
YasennT <tashkovyasen@gmail.com>
2023-11-25 15:32:54 +0200

    Main -> red 47 -> sell() ???

--- src/Main.java
+++ src/Main.java
@@ -2,39 +2,39 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         Bookstore bookstore = new Bookstore();
 
         System.out.print("Vavedete broi knigi: ");
-        int numberBooks = scanner.nextInt();
+        int numberBooks = sc.nextInt();
 
         for (int i = 0; i < numberBooks; i++) {
             System.out.println("Vavedete detaili za Kniga " + (i + 1));
 
             System.out.print("Vavedete zaglavie: ");
-            String title = scanner.next();
+            String title = sc.next();
 
             System.out.print("Vavedete imeto na avtora: ");
-            String authorName = scanner.next();
+            String authorName = sc.next();
 
             System.out.print("Vavedete vuzrast na avtor: ");
-            int authorAge = scanner.nextInt();
+            int authorAge = sc.nextInt();
 
             Author author = new Author(authorName, authorAge);
 
             System.out.print("Vavedete vid kniga (CHILDREN_BOOK, COMIC_BOOK, BOOK): ");
-            BookType bookType = BookType.valueOf(scanner.next());
+            BookType bookType = BookType.valueOf(sc.next());
 
             Book book;
             switch (bookType) {
                 case CHILDREN_BOOK:
                     System.out.print("Vavedete vuzrast: ");
-                    int age = scanner.nextInt();
+                    int age = sc.nextInt();
                     book = new ChildrenBook(title, author, 12.0, age);
                     break;
                 case COMIC_BOOK:
                     System.out.print("Enter character name: ");
-                    String characterName = scanner.next();
+                    String characterName = sc.next();
                     book = new ComicBook(title, author, 10.0, characterName);
                     break;
                 default:
@@ -46,8 +46,10 @@ public class Main {
             bookstore.addBook(book);
         }
 
-        for (int i = 1; i < numberBooks; i += 2) {      //Ima problem s metoda
-            bookstore.sell(bookstore.getBooks().get(i));
+        for (int i = 1; i < numberBooks; i += 2)  //Ima problem s metoda
+        {
+            Book prodajba = bookstore.getBooks().get(i);
+            bookstore.sell(prodajba);
         }
     }
 }
